26c5b507233c8b48933fa04905bc5b394cffbd65
alex <alex_is_ok@yahoo.com>
2018-11-26 10:56:27 +0200

    Crazy Pony App Carcass

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
+load 'logic'
+
+@pet = Pet.new
+@zero_point = @pet.actions(indicators: [5, 5, 5, 5])
+
 
 get '/' do
   erb :index
 end
 
+post '/page' do
+  @name = params[:name]
+  erb :page
+end
+
 get '/page' do
-  'Ok.. now U here'
+  erb :page
+end
+
+not_found do
+  status 404
+  erb :'404'
 end
